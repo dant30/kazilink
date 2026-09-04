27786b81318b92f6ed143d796c2eb3977e8ef8ea
Dant30 <dankarish.dk@gmail.com>
2026-09-04 19:06:45 +0300

    mpesa and sms intergration

--- backend/django/config/settings/production.py
+++ backend/django/config/settings/production.py
@@ -1,6 +1,11 @@
 from .base import *
+from django.core.exceptions import ImproperlyConfigured
 
 DEBUG = False
+
+if len(SECRET_KEY) < 50 or len(set(SECRET_KEY)) < 5 or SECRET_KEY.startswith('django-insecure-'):
+	raise ImproperlyConfigured('SECRET_KEY must be at least 50 characters and must not use a django-insecure value in production.')
+
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
